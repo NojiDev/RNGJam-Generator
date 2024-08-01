bc9a05a270cc4711a95049280784225a8579ca6f
NojiDev <nojama@protonmail.com>
2024-08-01 19:24:55 +0000

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,16 +1,39 @@
-let rerolls = 2;
+let max_rolls = 2;
+console.log(localStorage.getItem("rerollsLeft"))
+
+if (localStorage.getItem("rerollsLeft") == null){
+    localStorage.setItem("rerollsLeft", max_rolls)
+};
+
+
+
 let possible_themes = ["Sea horse","Salmon","Crab","Tuna"];
 
+function pageLoad() {
+    document.getElementById("generated word").innerHTML = localStorage.getItem("chosenTheme")
+
+    if (localStorage.getItem("rerollsLeft") == 0){
+        document.getElementById("reroll button").innerHTML = "No rerolls left"
+    }
+}
+
 function generateNumber() {
-    if (rerolls >= 1){
+    let rerolls_left = localStorage.getItem("rerollsLeft")
+    
+    console.log(rerolls_left)
+    if (rerolls_left >= 1){
+        rerolls_left -= 1
 
         let randomNumber = Math.floor(Math.random() * possible_themes.length);
-        console.log(possible_themes[randomNumber]);
 
-        rerolls -= 1;
 
-        document.getElementById("generated word").innerHTML = String(possible_themes[randomNumber])
-        document.getElementById("reroll button").innerHTML = "Rerolls left: " + String(rerolls)
+        localStorage.setItem("rerollsLeft", rerolls_left)
+        localStorage.setItem("chosenTheme", possible_themes[randomNumber])
+
+        let chosen_theme = localStorage.getItem("chosenTheme")
+
+        document.getElementById("generated word").innerHTML = chosen_theme
+        document.getElementById("reroll button").innerHTML = "Rerolls left: " + String(rerolls_left)
 
 
     };
